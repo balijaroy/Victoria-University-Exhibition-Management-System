91d561277d17613b78017bdd395c53a721ac4aa7
balijaroy <balijaroy@gmail.com>
2025-05-17 23:05:57 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,84 @@
+<html>
+<head>
+    <title>Victoria University Exhibition System</title>
+</head>
+<div style="border-style:solid;border-color:red;padding-bottom:50px;">
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+        body {
+            background-color: #FBF4E2;
+            font-family: Arial, sans-serif;
+        }
+        h1 {
+            text-align: center;
+        }
+        ul {
+            text-align: center;
+            list-style: none;
+            padding: 0;
+        }
+        ul li {
+            display: inline-block;
+            margin: 20px;
+        }
+        ul li a {
+            text-decoration: none;
+            color: #333;
+            font-size: 15 px;
+            display: inline-flex;
+            align-items: center;
+
+        }
+        ul li a img {
+            height: 24px;
+            width: 24px;
+        }
+		.UniversityLogo{
+			padding-left:37%;
+			padding-top:100px;
+			
+		}
+    </style>
+</head>
+<body>
+	<img class="UniversityLogo" src = "icons/vuu.png">
+    <h1>Victoria University Exhibition Management System</h1>
+    <ul>
+        <li>
+            <a href="registerGuest.php">
+                <img src="icons/guest-512.png">
+                Register Guest
+            </a>
+        </li>
+        <li>
+            <a href="manageEvents.php">
+                <img src="icons/events.png" >
+                Manage Exhibitions
+            </a>
+        </li>
+		<li>
+            <a href="addSessions.php">
+                <img src="icons/add.png" >
+                Manage Sessions
+            </a>
+        </li>
+        <li>
+            <a href="bookSeat.php">
+                <img src="icons/seat.png" >
+                Book a Seat
+            </a>
+        </li>
+        <li>
+            <a href="reports.php">
+                <img src="icons/reports.png" >
+                Reports
+            </a>
+        </li>
+    </ul>
+</body>
+</html>
+
+<div>
+</html>
